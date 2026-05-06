c9e854dcd8935366470d3276feb4f96280f5a2e7
Rocio-Farias <rafariasgigena@escuelasproa.edu.ar>
2026-05-06 10:36:18 -0300

    termine el codigo y corregi los errores

--- consumo_de_agua.py
+++ consumo_de_agua.py
@@ -1,12 +1,19 @@
+
 def calcular_objetivo_ml(peso_kg, nivel_actividad):
-    objetivo = peso_kg * 35
+    objetivo_base = peso_kg * 35 
 
     if nivel_actividad == "bajo":
-        objetivo *= 0.9
+        objetivo = objetivo_base * 0.9
+    elif nivel_actividad == "medio":
+        objetivo = objetivo_base
     elif nivel_actividad == "alto":
-        objetivo *= 1.1
+        objetivo = objetivo_base * 1.1
+    else:
+        objetivo = objetivo_base  # por seguridad
 
     return objetivo
+
+
 
 def estado_hidratacion(consumo_ml, objetivo_ml):
     if consumo_ml < objetivo_ml:
@@ -19,14 +26,16 @@ def estado_hidratacion(consumo_ml, objetivo_ml):
         exceso = consumo_ml - objetivo_ml
         porcentaje = (exceso / objetivo_ml) * 100
         return f"Has excedido tu objetivo en {porcentaje:.2f}%"
-    
+
+
+
 personas = []
 
 while True:
     try:
         print("\n--- Nueva persona ---")
 
-        peso = float(input("Ingrese el peso en kg: "))
+        peso = float(input("Ingrese el peso: "))
 
         while True:
             actividad = input("Ingrese nivel de actividad (bajo / medio / alto): ")
@@ -40,10 +49,34 @@ while True:
         objetivo = calcular_objetivo_ml(peso, actividad)
         estado = estado_hidratacion(consumo, objetivo)
 
-    
-        objetivo = calcular_objetivo_ml (peso, actividad)
-        estado= estado_hidratacion (consumo,  objetivo)
-        
-        print("Objetivo diario: {objetivo:.2f} ml")
-        print("Estado:", estado)
+        print(f"Objetivo diario: {objetivo:.2f} ml")
+        print(f"Estado: {estado}")
+
+       
+        persona = {
+            "peso": peso,
+            "actividad": actividad,
+            "consumo": consumo,
+            "objetivo": objetivo
+        }
+
+        personas.append(persona)
+
+     
+        seguir = input("¿Desea cargar otra persona? (si/no): ")
+        if seguir != "si":
+            break
+
+    except ValueError:
+        print("Error: Debe ingresar valores numéricos válidos.")
+
+
+
+print("\n=== RESUMEN DE PERSONAS ===")
+for i, p in enumerate(personas, start=1):
+    print(f"\nPersona {i}:")
+    print(f"Peso: {p['peso']}")
+    print(f"Actividad: {p['actividad']}")
+    print(f"Consumo: {p['consumo']} ml")
+    print(f"Objetivo: {p['objetivo']:.2f} ml")
      
